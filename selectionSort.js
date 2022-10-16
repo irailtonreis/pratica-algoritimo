39b6f5b6e5ac595ed53af6085e63d0308e6769ae
irailton.oliveira@driven.cx <irailton.oliveira91@gmail.com>
2022-10-16 09:00:00 -0300

    feat: Refatorando para usar foreach

--- selectionSort.js
+++ selectionSort.js
@@ -1,17 +1,13 @@
 const livros = require('./listaLivros')
 const menorValor = require('./menorValor')
 
-for (let atual = 0; atual < livros.length; atual++) {
-    let menor = menorValor(livros, atual)
-
-    let livroAtual = livros[atual]
-    console.log("posicao atual", atual)
-    console.log("Livro atual ", livros[atual])
+livros.forEach((_, indice) => {
+    let menor = menorValor(livros, indice)
+    let livroAtual = livros[indice]
     let livroMenorPreco = livros[menor]
-    console.log("Livro menor preço ", livros[menor])
-    livros[atual] = livroMenorPreco
+    livros[indice] = livroMenorPreco
     livros[menor] = livroAtual
-    
-}
+})
+
 
 console.log("livros", livros)
