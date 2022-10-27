59395575816be426ef632090e8b9fc746d037677
irailtonreis <irailton.oliveira91@gmail.com>
2022-10-27 08:38:30 -0300

    feat: Criando função troca

--- insertionSort.js
+++ insertionSort.js
@@ -4,7 +4,7 @@ function insertionSort(lista){
     for (let atual = 1; atual < lista.length; atual++) {
         let analise = atual
 
-        while (lista[analise].preco < lista[analise - 1].preco) {
+        while (analise > 0 && lista[analise].preco < lista[analise - 1].preco) {
 
             let itemAnalise = lista[analise]
             let itemAnterior = lista[analise - 1]
